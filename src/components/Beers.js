82bd001a1a5ae545c2532d5d865f8ae1835b8612
Toth <toth.balazs.roland@gmail.com>
2018-11-29 13:38:45 +0100

    ...

--- src/components/Beers.js
+++ src/components/Beers.js
@@ -14,7 +14,7 @@ export class Beers extends React.Component{
     componentDidMount(){
         this.setState( {isLoading: true});
 
-        fetch("http://localhost:8080/api/allbeers")
+        fetch("http://192.168.162.37:8080/api/beers")
             .then(response => response.json())
             .then(data => this.setState( {beers: data, isLoading:false}))
     }
@@ -26,8 +26,28 @@ export class Beers extends React.Component{
             return <div>Loading....</div>
         }
         else
-            return <div>
-                { beers.map((beer) => <li>{beer.name}</li>)}
+            return <div className="container ">
+                <div className="main">
+                    <div><h2 align="center">All beers</h2></div>
+                    { beers.map((beer, key) => <div key={beer.id} className="card">
+                    <div className="card-body">
+                        <h5 className="card-title"><a href={"/beer/"+beer.id}>{beer.name}</a></h5>
+                        <p className="brewery-name"> {beer.brewery}</p>
+                        <p className="beer-style"> {beer.type}</p>
+                        <p className="card-text">{beer.description}</p>
+                        <table className="table table-bordered">
+                            <tbody>
+                                <tr>
+                                    <td className="ABV"> {beer.abv} % ABV</td>
+                                    <td className="IBU">{beer.ibu} IBU</td>
+                                    <td className="rating"> Rating: {beer.rating} </td>
+                                    <td className="numberofratings">{beer.numberOfRatings} Ratings</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>)}
+                </div>
             </div>
     }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+
 export class Home  extends React.Component{
     render() {
         return (
-            <div>
-                <h1>tHIS IS HOME!</h1>
+            <div className="mainPage">
+                <img src="http://harmonyvalleycreamery.com/wp-content/uploads/2017/08/beer-2370783_1920.jpg"/>
+                <h1 className="centered">hUntapped - Drink, check in, rate and share with your friends!</h1>
             </div>
         );
     }
