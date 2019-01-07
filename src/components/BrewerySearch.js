719c3d5c41c2221d079fd8c5f4d02b72c38f8c15
Clethcriss <balazs.borsi.1986@gmail.com>
2019-01-07 13:40:22 +0100

    Funcion calling is fixed in BrewerySearch.js

--- src/components/BrewerySearch.js
+++ src/components/BrewerySearch.js
@@ -13,7 +13,7 @@ export class BrewerySearch extends React.Component {
     };
 
     sendSearch(nameChunk) {
-        const url = getUrl+"api/brewery/search=" + nameChunk;
+        const url = getUrl()+"api/brewery/search=" + nameChunk;
         fetch(url)
             .then(response => response.json())
             .then(data => {
